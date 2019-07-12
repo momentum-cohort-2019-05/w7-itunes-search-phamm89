a939e7ca9bcb9d52e6634c5439376e6a1ea513c6
Michael <phamm89@gmail.com>
2019-07-11 20:13:33 -0400

    Function for Enter=Click

--- main.js
+++ main.js
@@ -1,10 +1,47 @@
-document.addEventListener('DOMContent Loaded', function() {
-    fetch('https://itunes-api-proxy.glitch.me/search?term=""media=music&entity=musicTrack"')
-        .then(function(response) {
-            return response.json()
-        })
-        .then(function(data){
-            // Create functions for populating music tracks
-        })
+// Shorthand function for calling document.querySelector
+function q (selector) {
+    return document.querySelector(selector)
 }
-)
+
+// Shorthand function for calling document.querySelectorAll
+function qAll (selector) {
+    return document.querySelectorAll(selector)
+}
+
+// Variables
+let input
+let searchURL
+const searchForm = q('#searchForm')
+const searchButton = q('#searchButton')
+const searchBar = q('#searchBar')
+
+// When user releases Enter key, act as if submit button has been clicked
+searchForm.addEventListener('keyup', function(event){
+    if(event.keyCode === 'Enter'){
+        event.preventDefault()
+        searchButton.click()
+    }
+})
+
+
+function getMusic(){
+// Write the stuff that gets music data
+}
+
+
+
+// Main execution
+document.addEventListener('DOMContent Loaded', function() {
+    searchForm.addEventListener('submit', function(event) {
+        event.preventDefault()
+        input = encodeURIComponent(searchBar.value)
+        searchURL = `https://itunes-api-proxy.glitch.me/search?term=${input}&media=music&entity=musicTrack`
+
+        fetch(URL)
+            .then(response => response.json())
+            .then(function (data) {
+                getMusic(data.results)
+                console.log(data.results[0])
+        })
+    })
+})
